5dfd1d17ad0421b1bf4e89c970731498f180f3c4
AlnoorMajzoob2019 <46705050+AlnoorMajzoob2019@users.noreply.github.com>
2019-03-15 10:43:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const express = require("express");
 const app = express();
 
 
-app.get('/test',(req,res)=>{
+app.get('/details',(req,res)=>{
+    
+    var artist = query.artist
+    var song = query.song
+    
     res.send('hi');
 });
 
